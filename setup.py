d311bc636ab6244fe21b95e34e7f707d5b58fba4
Louis Mallez <louis.mallez@epitech.eu>
2020-10-03 17:25:37 +0200

    Hotfix pylintrc requirements and setup

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     name='harpokrat_client_library',
     version='0.0.1',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.4',
+    install_requires=['requests', 'hclw']
 )
