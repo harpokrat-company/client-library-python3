7d1e3b85a05ccad2933535bf39491da2f594aed7
Louis Mallez <louis.mallez@epitech.eu>
2020-10-03 19:32:47 +0200

    update install_hclw.sh

--- install_hclw.sh
+++ install_hclw.sh
@@ -1,12 +1,12 @@
-git clone git@gitlab.com:harpokrat/hcl/hcl-core.git
-cd hcl-core
+git clone git@github.com:harpokrat-company/hcl.git
+cd hcl
 cmake .
 make
 sudo cp libhcl.so /usr/lib/libhcl.so.0
 sudo ldconfig
 cd ..
-rm -rf hcl-core
+rm -rf hcl
 
-git clone git@gitlab.com:harpokrat/hcl/hclw-python3.git
-pip3 install hclw-python3/
+git clone git@github.com:harpokrat-company/hclw-python3.git
+python3 -m pip install hclw-python3/
 rm -rf hclw-python3
