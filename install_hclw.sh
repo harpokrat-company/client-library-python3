72ab315f24c3d041f7e332f0bf2ea20b1e3df16b
Louis Mallez <louis.mallez@epitech.eu>
2020-10-26 17:38:14 +0100

    Add Github Test Action (#5)

--- install_hclw.sh
+++ install_hclw.sh
@@ -1,5 +1,6 @@
 git clone git@github.com:harpokrat-company/hcl.git
 cd hcl
+git checkout v0.0.1
 cmake .
 make
 sudo cp libhcl.so /usr/lib/libhcl.so.0
@@ -8,5 +9,6 @@ cd ..
 rm -rf hcl
 
 git clone git@github.com:harpokrat-company/hclw-python3.git
+git checkout v0.0.1
 python3 -m pip install hclw-python3/
 rm -rf hclw-python3
